0bbfdd8781aab9852bb09593c9209d2324d88f50
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-03-25 17:23:31 +0900

    :fix delete dist folder and update gitignore

--- dist/boards/boards.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { BoardsService } from './boards.service';
-export declare class BoardsController {
-    private readonly boardsService;
-    constructor(boardsService: BoardsService);
-    getHello(): string;
-}
--- dist/boards/boards.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { UsersService } from 'src/users/users.service';
-export declare class BoardsService {
-    private readonly usersService;
-    constructor(usersService: UsersService);
-    ServiceHello(): string;
-}
